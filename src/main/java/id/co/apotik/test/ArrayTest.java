5f4e35dbefaf0daafebf6713e57e0512b9bda893
seven4ce <46161669+seven4ce@users.noreply.github.com>
2022-02-11 13:59:46 +0700

    update code

--- src/main/java/id/co/apotik/test/ArrayTest.java
+++ src/main/java/id/co/apotik/test/ArrayTest.java
@@ -7,7 +7,31 @@ public class ArrayTest {
 		int[] arrayA = {20,10,40,10,30};
 		int[] arrayB = {30,10,70,50,30};
 		
+		int[] arrayC = new int[arrayA.length + arrayB.length];
+
+		int Index = 0;
+
+		for (int i : arrayA) {
+			arrayC[Index] = i;
+			Index++;
+		}
+		for (int i : arrayB) {
+			arrayC[Index] = i;
+			Index++;
+		}
 		
+		for (int i = 0; i < arrayC.length; i++) {
+			int temp=0;
+			for (int j = i+1; j < arrayC.length; j++) {
+				if (arrayC[i]>arrayC[j]) {
+					temp = arrayC[i];
+					arrayC[i]=arrayC[j];
+					arrayC[j]=temp;
+				}
+				
+			}
+			System.out.print(arrayC[i]+" ");
+		}
 
 
 	}
